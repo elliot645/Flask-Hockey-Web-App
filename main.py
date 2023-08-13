f3c9dcf8b80722af8bf55198a6f402c8f22e0bb4
ELLIOT THOBURN <elliot.weldon.thoburn@gmail.com>
2023-08-12 22:33:04 -0400

    changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 import os
-import psycopg2
+#import psycopg2
 
 # app = Flask(__name__)
 
